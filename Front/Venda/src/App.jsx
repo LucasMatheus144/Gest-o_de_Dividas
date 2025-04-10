adad4d7ee166f8e736753deb4b6095246a1b2805
Lucas Marques <lucasmatheussouza53@gmail.com>
2025-04-10 17:00:35 -0300

    10/04 LucasM

--- Front/Venda/src/App.jsx
+++ Front/Venda/src/App.jsx
@@ -1,12 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Compartilhado from './Layout/Compartilhado';
+import Home from './pages/Home';
+
+import './global.css';
 
 export default function App() {
 
   return (
-    <>
-      
-     
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Compartilhado />}>
+          <Route index element={<Home />} />
+          {/* Exemplo comentado de rotas adicionais: */}
+          {/* <Route path="alunos" element={<ListaAlunos a={0} texto="TEXT" seila={true} />} /> */}
+          {/* <Route path="alunos/criar" element={<FormAluno />} /> */}
+          {/* <Route path="alunos/editar/:codigo" element={<FormAluno />} /> */}
+          {/* <Route path="cursos" element={<CursosList />} /> */}
+          <Route path="*" element={<h1>404 - NOT FOUND</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
---
+++ Front/Venda/src/Components/Nav/Navbar.jsx
@@ -0,0 +1,12 @@
+import styles from'./Navbar.module.css'
+
+export default function Navbar(){
+    return <>
+        <section class={styles.sec}>
+            <img src="" alt="seila" />
+            <div class={styles.box}>
+                <h3>Vendinha</h3>
+            </div>
+        </section>
+    </>
+}
